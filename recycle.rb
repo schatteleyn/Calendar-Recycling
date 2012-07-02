6fbc30993f5ca6d2c22752636e7c5e5fe9169d47
Simon Chatteleyn <kilik2049@gmail.com>
2012-07-02 14:30:40 +0200

    start adaptation of java code

--- recycle.rb
+++ recycle.rb
@@ -1,4 +1,14 @@
 require 'date'
 
 def same(year1, year2)
+  cb = Date.new(year1,1,1) 
+  pb = Date.new(year2,1,1)
+  ce = Date.new(year1,12,31)
+  pe = Date.new(year2,12,31)
+
+  if cb.wday == pb.wday && ce.wday == pe.wday
+    return true
+  else
+  	return false
+  end
 end
