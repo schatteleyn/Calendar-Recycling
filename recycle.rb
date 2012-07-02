17da928102e385c99ee8fee091a5367cd736bdfb
Simon Chatteleyn <kilik2049@gmail.com>
2012-07-02 18:33:54 +0200

    first step done. Have the equivalent years for the 10y from the one entered

--- recycle.rb
+++ recycle.rb
@@ -1,14 +1,28 @@
 require 'date'
 
 def same(year1, year2)
-  cb = Date.new(year1,1,1) 
-  pb = Date.new(year2,1,1)
-  ce = Date.new(year1,12,31)
-  pe = Date.new(year2,12,31)
+  cb = Date.new(year1,1,1)    # current year begin date (first day of year) 
+  pb = Date.new(year2,1,1)    # past year begin date (first day of year) 
+  ce = Date.new(year1,12,31)  # current year end date (last day of year)  
+  pe = Date.new(year2,12,31)  # past year end date (last day of year) 
 
   if cb.wday == pb.wday && ce.wday == pe.wday
     return true
   else
   	return false
   end
+end
+
+puts "Choose a year: "
+year = gets.chomp.to_i
+
+# Give the equivalence for 10 years from the one entered.
+(year..year+10).each do |c|
+  print "#{c}: "
+  ((p=1900)..c-1).each do |p|
+    if same(c,p)
+    	print "#{p} "
+    end
+  end
+  puts ""
 end
